11502818bcdcb56b4406d7adc02cda67663fdff4
Rbee <rbenedict.maagma@gmail.com>
2025-03-03 14:30:20 +0800

    Add new file associations and update settings

---
+++ include/food_validation.h
@@ -0,0 +1,11 @@
+#ifndef FOOD_VALIDATION_H
+#define FOOD_VALIDATION_H
+
+// Function prototypes for validation
+int validateFoodName(const char *);
+int validateFoodType(const char *);
+int validateTimesEaten(const int *);
+int validateDateFirstTried(const char *);
+int isLeapYear(int);
+
+#endif  // FOOD_VALIDATION_H
