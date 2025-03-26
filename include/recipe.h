9125baae2cffb5c5c12593370c78ee244b2b6fbe
Rbee <rbenedict.maagma@gmail.com>
2025-03-26 16:51:04 +0800

    Add recipe display and search functionality

--- include/recipe.h
+++ include/recipe.h
@@ -4,7 +4,7 @@
 #include "data_structures.h"
 
 // Function prototypes for adding of recipe
-void addRecipe(Recipe *);
+void addRecipe(Recipe[], int *, UserCredentials);
 
 // Function prototypes to get specific recipe details
 void getRecipeName(Recipe *);
@@ -15,5 +15,21 @@ void getNumOfIngredients(Recipe *);
 void getIngredients(Recipe *);
 void getNumOfInstructions(Recipe *);
 void getInstructions(Recipe *);
+
+// Function prototypes to display recipe details
+void displayRecipeDetails(Recipe *);
+void displayAllRecipes(Recipe[], int);
+
+// Function prototype to wrap recipe text
+void wrapRecipeText(const char *, int, int);
+
+// Function prototypes to modify, delete, and search recipe logs
+void searchRecipes(Recipe[], int);
+void modifyRecipe(Recipe[], int *);
+void deleteRecipe(Recipe[], int *);
+void searchRecipe(Recipe[], int);
+
+// Function prototype to find the index of a recipe
+int findRecipeIndex(Recipe[], int, char[]);
 
 #endif  // RECIPE_H
