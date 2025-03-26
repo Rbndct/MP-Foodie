9fc2c69172aefda57bb235ebf2cdd5700e439ce1
Rbee <rbenedict.maagma@gmail.com>
2025-03-26 16:51:37 +0800

    Add UserCredentials parameter to addFoodLog function and add new functions for displaying and searching food logs

--- include/food_log.h
+++ include/food_log.h
@@ -4,7 +4,7 @@
 #include "data_structures.h"
 
 // Function prototype to add a new food log entry
-void addFoodLog(FoodLog[], int *);
+void addFoodLog(FoodLog[], int *, UserCredentials);
 
 // Function prototypes to get specific food log details
 void getFoodName(FoodLog *);
@@ -14,11 +14,19 @@ void getDateFirstTried(FoodLog *);
 void getLocationFirstTried(FoodLog *);
 void getFoodDescription(FoodLog *);
 
-void displayAllFoodLogs(FoodLog[], int);
-void wrapText(const char *, int);
-
-void modifyFoodLog(FoodLog[], int *);
+// Function prototypes to display food log entries
 void displayFoodLogDetails(FoodLog *);
+void displayAllFoodLogs(FoodLog[], int);
+
+// Function prototype to wrap food log text
+void wrapFoodText(const char *, int);
+
+// Function prototypes to modify, delete, and search food logs
+void modifyFoodLog(FoodLog[], int *);
 void deleteFoodLog(FoodLog[], int *);
 void searchFoodLog(FoodLog[], int);
+
+// Function prototype to find the index of a food log
+int findFoodLogIndex(FoodLog[], int, char[]);
+
 #endif  // FOOD_LOG_H
