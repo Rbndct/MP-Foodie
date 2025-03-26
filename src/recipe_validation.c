a45b391d740b5c11e38e3d2ae4fdc2dcb7df6d35
Rbee <rbenedict.maagma@gmail.com>
2025-03-26 16:50:33 +0800

    Refactor input validation functions to handle input length overflow

--- src/recipe_validation.c
+++ src/recipe_validation.c
@@ -6,43 +6,63 @@
 
 #include "data_structures.h"
 
-int validateRecipeName(const char *recipe_name)
+int validateRecipeName(char *recipe_name)
 {
-    // Get recipe name length
-    int recipeName_len = strlen(recipe_name);
     int isValid = 1;
-
-    // Check if recipe name is between 3 and 50 characters
-    if (recipeName_len < 3 || recipeName_len > 50)
+    int len = strlen(recipe_name);
+    if (len < 3 || len > MAX_RECIPE_NAME_LEN - 1)
     {
-        isValid = 0;
         printf("Recipe name should be between 3 and 50 characters.\n");
+        isValid = 0;
     }
-
-    // Check if recipe name contains only alphanumeric characters
-    for (int i = 0; i < recipeName_len; i++)
+    int i = 0;
+    while (i < len && isValid)
     {
         if (!isalnum(recipe_name[i]) && recipe_name[i] != ' ')
         {
             printf("Recipe name should only contain alphanumeric characters.\n");
             isValid = 0;
         }
+        i++;
+    }
+    int ch, overflow = 0;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        overflow = 1;
+    }
+    if (overflow)
+    {
+        printf("Input was too long! Only 50 characters allowed.\n");
+        recipe_name[0] = '\0';
+        isValid = 0;
     }
     return isValid;
 }
 
-int validateRecipeDescription(const char *recipe_description)
+int validateRecipeDescription(char *recipe_description)
 {
-    // Get recipe description length
-    int recipeDescription_len = strlen(recipe_description);
     int isValid = 1;
+    int len = strlen(recipe_description);
 
-    // Check if recipe description is between 1 and 160 characters
-    if (recipeDescription_len < 0 || recipeDescription_len > 160)
+    if (len < 1 || len > MAX_RECIPE_DESCRIPTION_LEN - 1)
     {
-        isValid = 0;
         printf("Recipe description should be between 1 and 160 characters.\n");
+        isValid = 0;
     }
+
+    int ch, overflow = 0;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        overflow = 1;
+    }
+
+    if (overflow)
+    {
+        printf("Input was too long! Only 160 characters allowed.\n");
+        recipe_description[0] = '\0';
+        isValid = 0;
+    }
+
     return isValid;
 }
 
@@ -85,17 +105,28 @@ int validateNumOfIngredients(const int *num_of_ingredients)
     return isValid;
 }
 
-int validateIngredient(const char *ingredient)
+int validateIngredient(char *ingredient)
 {
-    // Get ingredient length
-    int ingredient_len = strlen(ingredient);
     int isValid = 1;
+    int len = strlen(ingredient);
 
-    // Check if ingredient is between 1 and 80 characters
-    if (ingredient_len < 0 || ingredient_len > 80)
+    if (len < 1 || len > MAX_INGREDIENT_LEN - 1)
     {
+        printf("Ingredient should be between 1 and 80 characters.\n");
         isValid = 0;
-        printf("Ingredient should be between 3 and 80 characters.\n");
+    }
+
+    int ch, overflow = 0;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        overflow = 1;
+    }
+
+    if (overflow)
+    {
+        printf("Input was too long! Only 80 characters allowed.\n");
+        ingredient[0] = '\0';
+        isValid = 0;
     }
 
     return isValid;
@@ -114,17 +145,28 @@ int validateNumOfInstructions(const int *num_of_instructions)
     return isValid;
 }
 
-int validateInstruction(const char *instruction)
+int validateInstruction(char *instruction)
 {
-    // Get instruction length
-    int instruction_len = strlen(instruction);
     int isValid = 1;
+    int len = strlen(instruction);
 
-    // Check if instruction is between 1 and 100 characters
-    if (instruction_len < 0 || instruction_len > MAX_INSTRUCTION_LEN - 1)
+    if (len < 1 || len > MAX_INSTRUCTION_LEN - 1)
     {
+        printf("Instruction should be between 1 and %d characters.\n", MAX_INSTRUCTION_LEN - 1);
         isValid = 0;
-        printf("Instruction should be between 1 and 100 characters.\n");
+    }
+
+    int ch, overflow = 0;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        overflow = 1;
+    }
+
+    if (overflow)
+    {
+        printf("Input was too long! Only %d characters allowed.\n", MAX_INSTRUCTION_LEN - 1);
+        instruction[0] = '\0';
+        isValid = 0;
     }
 
     return isValid;
