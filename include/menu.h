665108e99a27fc5a2227ab2cb92de33d3d9665e8
Rbee <rbenedict.maagma@gmail.com>
2025-03-26 16:51:24 +0800

    Update menu.h to include data_structures.h and modify function prototypes

--- include/menu.h
+++ include/menu.h
@@ -1,11 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
+#include "data_structures.h"
 
+// Function prototypes for displaying menus
 void displayMainMenu();
-void displayUserSubMenu();
-void displayFoodLogSubMenu(FoodLog *, int *);
-void displayRecipeSubMenu();
+void displayUserSubMenu(FoodLog[], int, Recipe[], int, UserCredentials *);
+
+// Function prototypes for displaying sub-menus
+void displayFoodLogSubMenu(FoodLog[], int *, UserCredentials);
+void displayRecipeSubMenu(Recipe[], int *, UserCredentials);
 void displayExportImportSubMenu();
+
+// Function prototype for displaying exit menu
 int displayExitMenu();
 
 #endif  // MENU_H
