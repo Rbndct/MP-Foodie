6e2397348ce3ce720f44e26e85feacc0353bc16d
Rbee <rbenedict.maagma@gmail.com>
2025-03-26 16:51:12 +0800

    Refactor function prototypes in recipe_validation.h

--- include/recipe_validation.h
+++ include/recipe_validation.h
@@ -2,13 +2,13 @@
 #define RECIPE_VALIDATION_H
 
 // Function prototypes for validation
-int validateRecipeName(const char *);
-int validateRecipeDescription(const char *);
+int validateRecipeName(char *);
+int validateRecipeDescription(char *);
 int validateTimeToPrepare(const int *);
 int validateTimeToCook(const int *);
 int validateNumOfIngredients(const int *);
-int validateIngredient(const char *);
+int validateIngredient(char *);
 int validateNumOfInstructions(const int *);
-int validateInstruction(const char *);
+int validateInstruction(char *);
 
 #endif  // RECIPE_VALIDATION_H
